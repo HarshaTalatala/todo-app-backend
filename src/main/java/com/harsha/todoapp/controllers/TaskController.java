7ddfab5aa711553bb873d4ffc5ba2f49b9865f17
HarshaTalatala <harsha.talatala@gmail.com>
2025-09-28 22:50:07 +0530

    Todo-App : Java Backend

---
+++ src/main/java/com/harsha/todoapp/controllers/TaskController.java
@@ -0,0 +1,60 @@
+package com.harsha.todoapp.controllers;
+
+import com.harsha.todoapp.models.Task;
+import com.harsha.todoapp.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Task>> getAllTasks(){
+        return ResponseEntity.ok(taskService.getAllTask());
+    }
+
+    @GetMapping("/completed")
+    public ResponseEntity<List<Task>> getAllCompletedTasks(){
+        return ResponseEntity.ok(taskService.findAllCompletedTask());
+    }
+
+    @GetMapping("incompleted")
+    public ResponseEntity<List<Task>> GetAllIncompletedTasks(){
+        return ResponseEntity.ok(taskService.findAllIncompletedTask());
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<Task> createTask(@RequestBody Task task){
+        return ResponseEntity.ok(taskService.createNewTask(task));
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task){
+        task.setId(id);
+        return ResponseEntity.ok(taskService.updateTask(task));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return ResponseEntity.ok(true);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Task>> getTaskById(@PathVariable Long id){
+        return ResponseEntity.ok(taskService.findTaskById(id));
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Controller is working!";
+    }
+}
---
+++ src/main/java/com/harsha/todoapp/models/Task.java
@@ -0,0 +1,22 @@
+package com.harsha.todoapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String task;
+    private boolean completed;
+
+}
